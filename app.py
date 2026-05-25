0da0c6b0e22346c281f130cb6adebf06820d4446
Ryan Gosling <vjlev671games@gmail.com>
2026-05-25 22:47:27 +0500

    ...

--- app.py
+++ app.py
@@ -208,5 +208,12 @@ def free_slot():
 
 
 if __name__ == "__main__":
+
     logger.info("Сервер запущен")
-    app.run(host="0.0.0.0", port=5000)
+
+    port = int(os.environ.get("PORT", 10000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
